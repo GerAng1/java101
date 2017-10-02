52db40879d2750c412d850a0fca891e812c5e5bc
GerAng1 <ganglada.dlanda@gmail.com>
2017-10-02 12:08:18 -0500

    Nuevo Código

---
+++ For.java
@@ -0,0 +1,21 @@
+/********************************|
+* | Fundamentos de Programación  |
+* | "Condiciones con For"        |
+* | Gerardo Anglada - A01021917  |
+* | 2/Oct/2017                   |
+* |******************************/
+
+public class For {
+
+public static void main(String[] args) {
+
+for(int x=1; x<40;x++){
+  System.out.print(x);
+  if(x%2==0){
+   System.out.print("es par");
+  }
+  System.out.println();
+  }
+
+  }
+}

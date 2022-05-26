fefb8d2eee9540985f454078f5f639d991184a0a
Gerardo Anglada <ganglada.dlanda@gmail.com>
2022-05-26 13:25:01 +0200

    Created projects/ directory & added project there.

---
+++ projects/Bancooo/Fecha.java
@@ -0,0 +1,50 @@
+package bancooo;
+
+public class Fecha {
+  private int dia;
+  private int mes;
+  private int anno;
+
+  public Fecha(int mes, int anio) throws ArithmeticException {
+    if (mes < 1 || mes > 12)
+      throw new ArithmeticException();
+
+    this.mes = mes;
+    anno = anio;
+  }
+
+  public Fecha(int dia, int mes, int anio) throws ArithmeticException {
+    if (mes < 1 || mes > 12)
+      throw new ArithmeticException("Los meses van del 1 al 12. Vuelva a intenarlo.");
+
+    if ((dia < 1 || dia > 31))
+      throw new ArithmeticException("Los dias van del 1 al 31. Vuelva a intenarlo.");
+
+    if (mes == 2 && dia > 29)
+      throw new ArithmeticException("Febrero no puede tener más de 29 días.");
+
+    this.dia = dia;
+    this.mes = mes;
+    anno = anio;
+  }
+
+  public String FormatoFecha() {
+    return String.format("%02d/%d", mes, anno);
+  }
+
+  public String FormatoFecha2() {
+    return String.format("%02d/%02d/%d", dia, mes, anno);
+  }
+
+  public int getDia(){
+    return dia;
+  }
+
+  public int getMes() {
+    return mes;
+  }
+
+  public int getAnno() {
+    return anno;
+  }
+}
